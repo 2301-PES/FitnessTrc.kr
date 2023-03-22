8794c3b7291b7f29cef0dd2306b60c3a587235ff
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-03-22 18:09:54 -0400

    initial branch talal setup and database boilerplate

---
+++ db/seedData.js
@@ -0,0 +1,10 @@
+const client = require("./client");
+
+async function dropTables(){
+    console.log("Dropping all tables...");
+    
+}
+async function createTables(){
+    console.log("Starting to build tables...")
+
+}
