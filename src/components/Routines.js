5c8b033f39206f8716bcb395dbb5acf297ac83c3
Harley Fugett <harleyfugett@gmail.com>
2023-03-23 15:25:58 -0600

    Routine start

---
+++ src/components/Routines.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+const allRoutines = async () => {
+
+    
+
+    try {
+        const response = await fetch(`${BASE_URL}/routines`, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const result = await response.json();
+        return result 
+
+    useEffect(() => {
+        
+    })
+
+    } catch (e) {
+        console.log(e);
+    }
+    return (
+
+    )
+}
+
+export default allRoutines;
