59d547fd2a1cd14421be565954c274e96dfba234
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-03-27 13:19:35 -0400

    changing client to deploy

--- db/client.js
+++ db/client.js
@@ -1,8 +1,9 @@
-// require('dotenv').config();
+require('dotenv').config();
 const { Client } = require('pg');
-// const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev'
-const connection = 'postgres://localhost:5432/fitness-dev'
+const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev'
+// const connection = 'postgres://localhost:5432/fitness-dev'
 const client = new Client(connection);
+client.password="CjhwkqG8NYoBUIt5l7cIYbSC1p959Xa4"
 
 module.exports = {
     client
